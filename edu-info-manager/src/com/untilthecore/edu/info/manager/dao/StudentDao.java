b64b46e632a2a6a8de3b14c656d67f1df7039c50
untilthecore <814898862@qq.com>
2022-04-22 00:58:31 +0800

    优化学生信息的增删改查功能

--- edu-info-manager/src/com/untilthecore/edu/info/manager/dao/StudentDao.java
+++ edu-info-manager/src/com/untilthecore/edu/info/manager/dao/StudentDao.java
@@ -5,25 +5,27 @@ import com.untilthecore.edu.info.manager.domain.Student;
 import java.util.ArrayList;
 
 public class StudentDao {
-  private final ArrayList<Student> students = new ArrayList<>();
+  private static final ArrayList<Student> students = new ArrayList<>();
 
   public boolean add(Student s) {
-    return this.students.add(s);
+    return students.add(s);
   }
 
-  public boolean remove(Student s) {
-    return this.students.remove(s);
+  public boolean remove(String id) {
+    Student student = findById(id);
+    return students.remove(student);
   }
 
-  public boolean update(Student s, String name, String gender, int age) {
-    s.setName(name);
-    s.setGender(gender);
-    s.setAge(age);
+  public boolean update(String id, String name, String gender, int age) {
+    Student student = findById(id);
+    student.setName(name);
+    student.setGender(gender);
+    student.setAge(age);
     return true;
   }
 
   public Student findById(String id) {
-    for(Student nextStudent : this.students) {
+    for(Student nextStudent : students) {
       if(nextStudent.getId().equals(id)) {
         return nextStudent;
       }
@@ -32,6 +34,6 @@ public class StudentDao {
   }
 
   public ArrayList<Student> findAll() {
-    return this.students;
+    return students;
   }
 }
--- edu-info-manager/src/com/untilthecore/edu/info/manager/service/StudentService.java
+++ edu-info-manager/src/com/untilthecore/edu/info/manager/service/StudentService.java
@@ -4,93 +4,55 @@ import com.untilthecore.edu.info.manager.dao.StudentDao;
 import com.untilthecore.edu.info.manager.domain.Student;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class StudentService {
-  private final StudentDao sDao = new StudentDao();
+  private final StudentDao studentDao = new StudentDao();
 
-  public void addStudent() {
-    Scanner scanner = new Scanner(System.in);
-    String id;
-    while (true) {
-      System.out.print("请输入学号:");
-      id = scanner.nextLine();
-      if (sDao.findById(id) != null) {
-        System.out.println("学号重复，请重新输入");
-      } else {
+
+  public boolean update(String id, String name, String gender, int age) {
+    return studentDao.update(id, name, gender, age);
+  }
+
+  public ArrayList<Student> findAll() {
+    return studentDao.findAll();
+  }
+
+  public boolean isExists(String id) {
+    StudentDao studentDao = new StudentDao();
+    ArrayList<Student> all = studentDao.findAll();
+    boolean exists = false;
+    for (Student student : all) {
+      if(student.getId().equals(id)) {
+        exists = true;
         break;
       }
     }
-
-    System.out.print("请输入姓名:");
-    String name = scanner.nextLine();
-    System.out.print("请输入性别:");
-    String gender = scanner.nextLine();
-    System.out.print("请输入年龄");
-    int age = scanner.nextInt();
-    if (sDao.add(new Student(id, name, age, gender))) {
-      System.out.println("学生信息添加成功!");
-    }
+    return exists;
   }
 
-  public void removeStudent() {
-    System.out.print("请输入待删除的学生ID:");
-    Scanner scanner = new Scanner(System.in);
-    String id = scanner.nextLine();
+  public boolean add(Student student) {
+    StudentDao studentDao = new StudentDao();
+    return studentDao.add(student);
+  }
 
-    Student student = sDao.findById(id);
-    if (student != null) {
-      if (sDao.remove(student)) {
-        System.out.println("ID:" + id + "删除成功");
-      } else {
-        System.out.println("删除失败");
+  public Student find(String id) {
+    boolean exists = this.isExists(id);
+    if(!exists) {
+      return null;
+    }
+
+    ArrayList<Student> all = this.findAll();
+    for (Student student : all) {
+      if(student.getId().equals(id)) {
+        return student;
       }
-    } else {
-      System.out.println("无此学生");
     }
+
+    return null;
   }
 
-  public void updateStudent() {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("请输入要修改的用户ID");
-    String id = scanner.nextLine();
-    Student student = sDao.findById(id);
-    if (student != null) {
-      System.out.println("请输入姓名");
-      String name = scanner.nextLine();
-      System.out.println("请输入性别");
-      String gender = scanner.nextLine();
-      System.out.println("请输入年龄");
-      int age = scanner.nextInt();
-      if(sDao.update(student, name, gender, age)) {
-        System.out.println("更新成功!");
-      }
-    } else {
-      System.out.println("查无此人");
-    }
-  }
-
-  public void findStudent() {
-    System.out.print("请输入学生学号:");
-    Scanner sc = new Scanner(System.in);
-    String id = sc.nextLine();
-    Student student = sDao.findById(id);
-    if (student != null) {
-      System.out.println("查到学生信息：" + student.toString());
-    } else {
-      System.out.println("查无此人");
-    }
-  }
-
-  public void findAll() {
-    ArrayList<Student> studentArr = sDao.findAll();
-    if (studentArr.size() > 0) {
-      System.out.println("查询到学生列表，容量:" + studentArr.size());
-      for (Student student : studentArr) {
-        System.out.println(student.toString());
-      }
-    } else {
-      System.out.println("暂无学生信息列表");
-    }
+  public boolean remove(String id) {
+    StudentDao studentDao = new StudentDao();
+    return studentDao.remove(id);
   }
 }
